377f3b0534b2b19a7c64d67aa1c444e9a71532ae
Arjun Chauhan <arjun99.chauhan@wesence.com>
2019-11-12 00:13:04 +0530

    fixes

---
+++ src/components/dropdown/DropDown.jsx
@@ -0,0 +1,130 @@
+// @flow
+import React from 'react';
+import Select from 'react-select';
+
+type Props = {
+  name: String,
+  id: String,
+  placeholder?: String,
+  options?: Array,
+  handler: () => void,
+  fluid?: Boolean,
+  theme?: Object,
+  width?: String,
+  autoFocus?: Boolean,
+  isMulti?: Boolean,
+  defaultValue?: Object | Array,
+  hideDropdownIndicator?: Boolean,
+  hideOptions?: Boolean,
+  onChange?: () => void,
+  value: Array<Object>,
+};
+
+const DropDown = ({
+  name,
+  id,
+  placeholder,
+  options,
+  handler,
+  fluid,
+  width,
+  theme,
+  autoFocus,
+  isMulti,
+  defaultValue,
+  hideDropdownIndicator,
+  hideOptions,
+  onChange,
+  value,
+}: Props) => {
+  const selectStyles = {
+    control: (provided, state) => ({
+      ...provided,
+      padding: 0,
+      borderWidth: 2,
+      borderColor: state.isFocused
+        ? theme.colors.btnPrimary
+        : theme.colors.textSecondaryLight,
+      boxShadow: 'unset',
+      borderRadius: 5,
+      marginBottom: 15,
+      width: fluid ? '100%' : 'auto',
+      maxWidth: width || 'unset',
+      transition: 'all 0.2s ease-in-out',
+      '&:hover': {
+        borderColor: state.isFocused
+          ? theme.colors.btnPrimary
+          : theme.colors.textSecondaryLight,
+      },
+      '&>div': {
+        overflow: 'unset',
+        '&:first-child': {
+          cursor: 'text',
+        },
+      },
+    }),
+    multiValue: (provided) => ({
+      ...provided,
+      backgroundColor: theme.colors.textSecondaryLight,
+      marginRight: 10,
+    }),
+    multiValueLabel: (provided) => ({
+      ...provided,
+      color: theme.colors.textPrimary,
+      paddingLeft: 8,
+    }),
+    multiValueRemove: (provided) => ({
+      ...provided,
+      cursor: 'pointer',
+      marginLeft: 3,
+      '&>svg': {
+        fill: theme.colors.textSecondary,
+      },
+      '&:hover': {
+        backgroundColor: theme.colors.btnPrimary,
+        '&>svg': {
+          fill: '#ffffff',
+        },
+      },
+    }),
+    menu: (provided) => ({
+      ...provided,
+      display: hideOptions ? 'none' : 'block',
+      maxWidth: width || 'unset',
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: state.isSelected ? theme.colors.btnPrimary : '#ffffff',
+      '&:focus': {
+        backgroundColor: state.isSelected
+          ? theme.colors.btnPrimaryHover
+          : theme.colors.btnPrimary,
+      },
+      '&:hover': {
+        backgroundColor: state.isSelected
+          ? theme.colors.btnPrimaryHover
+          : theme.colors.textSecondaryLight,
+      },
+    }),
+  };
+  return (
+    <>
+      <Select
+        theme={theme}
+        name={name}
+        id={id}
+        options={options}
+        styles={selectStyles}
+        onChange={onChange || ((r) => handler({ name, value: r }))}
+        placeholder={placeholder || 'Select...'}
+        autoFocus={autoFocus}
+        isMulti={isMulti}
+        defaultValue={defaultValue}
+        components={hideDropdownIndicator && { DropdownIndicator: null }}
+        value={value}
+      />
+    </>
+  );
+};
+
+export default DropDown;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { default as Input } from './components/input';
 export { default as DatePicker } from './components/datePicker';
+export { default as DropDown } from './components/dropdown';
