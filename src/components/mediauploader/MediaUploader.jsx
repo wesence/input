a8e05f8b6efd8c62dde9f66ee10bb4fe245249e8
Arjun Chauhan <arjun99.chauhan@wesence.com>
2019-11-13 14:10:52 +0530

    fixes

---
+++ src/components/mediauploader/MediaUploader.jsx
@@ -0,0 +1,40 @@
+// @flow
+import React from 'react';
+import { InputContainer } from '../input/Input.styled';
+
+type Props = {
+  value: String,
+  errors: Array<Object>,
+  handleUpload: () => void,
+};
+
+const MediaUploader = ({ errors, handleUpload, value }: Props) => {
+  const onChange = (e) => {
+    const file = e.target.files[0];
+    handleUpload(file);
+  };
+
+  return (
+    <InputContainer withError={errors && errors.length > 0}>
+      <input
+        type="file"
+        accept="image/*"
+        onChange={(e) => onChange(e)}
+        className="upload"
+      />
+      {value && (
+        <div className="image-container">
+          <img src={value} alt="preview" />
+        </div>
+      )}
+      {errors &&
+        errors.length > 0 &&
+        errors.map((i) => (
+          <span key={i.id} className="error">
+            {i.message}
+          </span>
+        ))}
+    </InputContainer>
+  );
+};
+export default MediaUploader;
